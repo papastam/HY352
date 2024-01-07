b05a78efa699b0ccdfaa7668ee3327e43526d078
Chris Papastamos <xristos.papastamos@gmail.com>
2024-01-07 22:25:23 +0200

    removed extra fields

--- inc/ability.h
+++ inc/ability.h
@@ -4,20 +4,14 @@
 class Ability{
     private: 
         char* name;
-        int damage; //negative damage is healing
-        int turns_to_cast;
-        int for_num_turns;
 
     public:
-        Ability(char* _name, int _damage, int _turns_to_cast, int _for_num_turns){
+        Ability(char* _name){
             name = _name;
-            damage = _damage;
-            turns_to_cast = _turns_to_cast;
-            for_num_turns = _for_num_turns;
         }
 
         void cast_ability(Pokemon* target){
-            target->setHp(target->getHp() - damage);
+            target->setHp(target->getHp());
         }
 
 };
